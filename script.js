09e76bd9f0d9bfc68d6ce480869eb252c6c710da
Alyse Bos <alyse.bos@gmail.com>
2018-06-13 19:39:31 -0700

    added data from additional api

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function getDataFromTasteDive (movieTitle, callback) {
 		k: '310324-SchoolPr-806Y2U29',
 	}
 	// now actually pull the data from the query, and run the callback...
-	$.ajax ({
+	$.ajax({
 		url: tasteDiveURL,
 		data: query,
 		dataType: 'jsonp',
@@ -18,63 +18,95 @@ function getDataFromTasteDive (movieTitle, callback) {
 	});
 }
 
+/*
 // Delete this eventually -- uses dummy data so I'm not pulling my API constantly
-// const tasteDiveData = tasteSample;
+const tasteDiveData = tasteSample;
 
 // Delete this eventually -- loads the dummy data similarly to how the getJSON will work
-// function getDataFromTasteDive (uselessArg, callback) {
-// 	console.log('getting data');
-// 	callback(tasteDiveData);
-// }
+function getDataFromTasteDive (uselessArg, callback) {
+	callback(tasteDiveData);
+}
+*/
+
+// set up the OMDB URL
+const omdbURL = 'https://www.omdbapi.com';
+
+// get data from OMDB
+function getDataFromOMDB (movieTitle, callback) {
+	const query = {
+		t: `${movieTitle}`,
+		type: 'movie',
+		apikey: '8d8c7d25',
+	}
+	$.ajax({
+		url: omdbURL,
+		data: query,
+		dataType: 'jsonp',
+		success: callback,
+	});
+}
+
+function apiErrorMessage (errReturned) {
+	$('.js-err-message').html(`
+		<p>There was a problem loading data. The following error message was displayed:</p>
+		<p>${errReturned}</p>
+		<p>Please try again later, or if the problem persists just give up and watch the movie you just searched for. You like it any way!</p>
+		<button class="restart-button js-error-go-back-button">
+			Go back and try again
+		</button>
+	`);
+	// hide the search screen
+	$('.search-screen').prop('hidden', true);
+	// show the error screen
+	$('.error-screen').prop('hidden', false);
+}
+
+function spellingErrorMessage (searchedString) {
+	$('.js-err-message').html(`
+		<p>I couldn't find any results matching ${searchedString}! Please go back and check your spelling or try again later.</p>
+		<button class="restart-button js-error-go-back-button">
+			Go back and try again
+		</button>
+	`);
+	// hide the search screen
+	$('.search-screen').prop('hidden', true);
+	// show the error screen
+	$('.error-screen').prop('hidden', false);
+}
+
+// Display data from OMDB
+function displayDataFromOMDB (data){
+
+}
 
 // Displays the data from the API
 function displayDataFromTasteDive (data) {
+	console.log(data);
 	// check if there is are no results
 	if (data.error) {
 		// set up error screen
-		$('.js-err-message').html(`
-			<p>There was a problem loading data. The following error message was displayed:</p>
-			<p>${data.error}</p>
-			<p>Please try again later, or if the problem persists just give up and watch the movie you just searched for. You like it any way!</p>
-			<button class="restart-button js-error-go-back-button">
-				Go back and try again
-			</button>
-		`);
-		// hide the search screen
-		$('.search-screen').prop('hidden', true);
-		// show the error screen
-		$('.error-screen').prop('hidden', false);
+		apiErrorMessage(data.error);
 	} else if (data.Similar.Results.length == 0) {
-		console.log(data);
 		// set up error screen stating no matches found
-		$('.js-err-message').html(`
-			<p>I couldn't find any results matching ${data.Similar.Info[0].Name}! Please go back and check your spelling or try again later.</p>
-			<button class="restart-button js-error-go-back-button">
-				Go back and try again
-			</button>
-		`);
-		// hide the search screen
-		$('.search-screen').prop('hidden', true);
-		// show the error screen
-		$('.error-screen').prop('hidden', false);
+		spellingErrorMessage(data.Similar.Info[0].Name);
 	} else {
 		// set up the results variable and start the function to map through each item of the array I need within the API results
-		const results = data.Similar.Results.map(item => {
-			// return the string which will be used for each list item
-			return `
-			<li class="col-6 movie-card">
-				<embed src="https://www.youtube.com/embed/${item.yID}">
-				<h3>${item.Name}</h3>
-				<p class="release-year">Release Year</p>
-				<p class="movie-plot">Movie plot</p>
-				<p class="movie-rating">IMDB Rating:</p>
-				<p class="rating-stars"><i>fa fa-star fa-star-half-o fa-star-o</i></p>
-				<p class="imdb-link">IMDB link</p>
-			</li>
-		`;
+		const results = data.Similar.Results.forEach(item => {
+			// Get this movie's data from OMDB
+			getDataFromOMDB(item.Name, function (data) {
+				//append all data into the <ul>
+				$('.js-movie-card-list').append(`
+					<li class="col-6 movie-card">
+						<embed src="https://www.youtube.com/embed/${item.yID}">
+						<h3>${item.Name}</h3>
+						<p class="movie-year">${data.Year}</p>
+						<p class="movie-plot">${data.Plot}</p>
+						<p class="movie-rating">${data.imdbRating}</p>
+						<p class="movie-link">${data.imdbID}</p>
+					</li>
+				`);
+			});
 		});
-		// insert the results into the card list
-		$('.js-movie-card-list').html(results);
 		// hide the search screen
 		$('.search-screen').prop('hidden', true);
 		// show the results screen
